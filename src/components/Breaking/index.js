1f9c29bffff396acce147374397c05a7a12ed3a9
Hayun <mycolki@gmail.com>
2021-10-04 03:33:41 +0900

    Feat: divide Breaking component  for layout.

--- src/components/Breaking/index.js
+++ src/components/Breaking/index.js
@@ -1,43 +1,27 @@
-import { Stage, Layer, RegularPolygon } from 'react-konva';
-
-import theme from '../../styles/theme';
-
-const stageStyle = {
-  height: '100%',
-  background: theme.mainSkyBlueBg,
-};
+import styled from 'styled-components';
+import AnswerDisplayBox from '../AnswerDisplayBox';
+import IcePlate from '../CubePlate';
+import Footer from '../Footer';
+import Header from '../Header';
+import InputBox from '../InputBox';
 
 function Breaking() {
   return (
-    <Stage style={stageStyle} width={350} height={350}>
-      <Layer>
-        <RegularPolygon
-          x={100}
-          y={100}
-          sides={6}
-          radius={25}
-          fillLinearGradientStartPoint={{
-            x1: 0,
-            y1: 0,
-          }}
-          fillLinearGradientEndPoint={{
-            x2: 0,
-            y2: 50,
-          }}
-          fillLinearGradientColorStops={[
-            0,
-            '#7cdffF',
-            1,
-            'rgba(207, 218, 255)',
-          ]}
-          shadowColor="#ffffff"
-          shadowBLur={0}
-          shadowOffset={{ x: 2, y: 1 }}
-          rotation={90}
-        />
-      </Layer>
-    </Stage>
+    <Container>
+      <Header />
+      <AnswerDisplayBox />
+      <IcePlate />
+      <InputBox />
+      <Footer />
+    </Container>
   );
 }
 
 export default Breaking;
+
+const Container = styled.div`
+  height: 100%;
+  background: ${({ theme }) => theme.mainSkyBlueBg};
+
+  border: 4px solid black;
+`;
---
+++ src/components/InputBox/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+function InputBox() {
+  return <Wrapper>InputBox</Wrapper>;
+}
+
+export default InputBox;
+
+const Wrapper = styled.div`
+  height: 12.5%;
+  background-color: yellowgreen;
+`;
