e924e3fa7507646dbf1f1d443d4bdacbf50a8557
Hayun <mycolki@gmail.com>
2021-10-04 18:20:50 +0900

    Feat: remove clicked cube

--- src/components/CubePlate/index.js
+++ src/components/CubePlate/index.js
@@ -9,61 +9,37 @@ function IcePlate() {
   const [cubeCoordinates, setCubeCoordinates] = useState([{ x: 0, y: 0 }]);
 
   useEffect(() => {
-    const coordinates = [];
-    let x = 0;
-    let y = 0;
+    const standards = [
+      [4, 181, 78],
+      [5, 142, 100],
+      [6, 103, 122],
+      [7, 64, 144],
+      [6, 64, 188],
+      [5, 64, 232],
+      [4, 64, 276],
+    ];
 
-    for (let i = 0; i < 4; i++) {
-      x = 181 + i * 39;
-      y = 78 + i * 22;
+    const makeCoordinates = (standards) => {
+      const coordinates = [];
 
-      coordinates.push({ x, y });
-    }
+      standards.forEach((standard) => {
+        for (let i = 0; i < standard[0]; i++) {
+          coordinates.push({
+            x: standard[1] + i * 39,
+            y: standard[2] + i * 22,
+          });
+        }
+      });
 
-    for (let i = 0; i < 5; i++) {
-      x = 142 + i * 39;
-      y = 100 + i * 22;
+      return coordinates;
+    };
 
-      coordinates.push({ x, y });
-    }
-
-    for (let i = 0; i < 6; i++) {
-      x = 103 + i * 39;
-      y = 122 + i * 22;
-
-      coordinates.push({ x, y });
-    }
-
-    for (let i = 0; i < 7; i++) {
-      x = 64 + i * 39;
-      y = 144 + i * 22;
-
-      coordinates.push({ x, y });
-    }
-
-    for (let i = 0; i < 6; i++) {
-      x = 64 + i * 39;
-      y = 188 + i * 22;
-
-      coordinates.push({ x, y });
-    }
-
-    for (let i = 0; i < 5; i++) {
-      x = 64 + i * 39;
-      y = 232 + i * 22;
-
-      coordinates.push({ x, y });
-    }
-
-    for (let i = 0; i < 4; i++) {
-      x = 64 + i * 39;
-      y = 276 + i * 22;
-
-      coordinates.push({ x, y });
-    }
-
-    setCubeCoordinates(coordinates);
+    setCubeCoordinates(makeCoordinates(standards));
   }, []);
+
+  const removeCube = (ev) => {
+    ev.target.visible(false);
+  };
 
   return (
     <Stage style={stageStyle} width={367} height={413.53}>
@@ -78,32 +54,34 @@ function IcePlate() {
           fillLinearGradientEndPoint={{ x: 150, y: 0, z: 0 }}
           fillLinearGradientColorStops={[
             0,
-            '#62A8F2',
+            '#62a8f2',
             0.5,
-            '#B7A4EE',
+            '#B7A4ee',
             1,
-            '#8BCFFC',
+            '#8bcffc',
           ]}
           shadowColor="#000000"
           shadowBlur={10}
           shadowOffset={{ x: 0, y: 10 }}
-          shadowOpacity="0.4"
+          shadowOpacity={0.4}
         />
-        {cubeCoordinates?.map((coord, index) => (
+        {cubeCoordinates?.map((coord) => (
           <RegularPolygon
+            key={String(coord.x) + String(coord.y)}
             x={coord.x}
             y={coord.y}
-            draggable="true"
             sides={6}
             radius={25}
             rotation={90}
             fillLinearGradientStartPoint={{ x: -20, y: 0 }}
             fillLinearGradientEndPoint={{ x: 20, y: 0 }}
-            fillLinearGradientColorStops={[0, '#E8E3FF', 1, '#BD9CF2']}
+            fillLinearGradientColorStops={[0, '#e8e3ff', 1, '#bd9cf2']}
             shadowColor="#000000"
             shadowBlur={4}
             shadowOffset={{ x: 1, y: 6 }}
-            shadowOpacity="0.2"
+            shadowOpacity={0.2}
+            onClick={removeCube}
+            onTouchEnd={removeCube}
           />
         ))}
       </Layer>
